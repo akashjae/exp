3fc7f846c72db0aa45836eeea4251b83f1b0ffab
akashjae <126228270+akashjae@users.noreply.github.com>
2023-04-23 21:56:53 +0530

    Create hey.java

---
+++ hey.java
@@ -0,0 +1,9 @@
+public class hey
+{
+	public static void main(String[] args) {
+          int x =5; //first arguments.
+          int y = 10; //second arguments.
+          int sum = x + y;
+          System.out.println("The sum of x and y is: " +sum);
+	}
+}
